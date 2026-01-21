c27b1d74287a18f0925c7b4f61ca5a2c98b08c5d
Yefan Shen <shenyefan0793@gmail.com>
2026-01-22 02:38:07 +0800

    Update app configuration with new email and description

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,9 @@
 export default defineAppConfig({
   title: 'Sink',
-  github: 'https://github.com/miantiao-me/sink',
-  twitter: 'https://sink.cool/x',
-  telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  email: 'shenyefan@syf.sh',
+  // twitter: 'https://sink.cool/x',
+  // telegram: 'https://sink.cool/telegram',
+  description: 'Meow',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
